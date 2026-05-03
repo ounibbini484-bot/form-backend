0ed2ef40bbbcd044020e760ff6ec18ffb9c3ca07
Jawwad Umar <jawwadumar99@gmail.com>
2026-05-03 12:33:52 +0530

    delete user added successfully

--- controller/userController.js
+++ controller/userController.js
@@ -45,3 +45,16 @@ export const updateUser = async(req, res) =>{
         res.status(500).json({ message: "Error updating user", error })
     }
 }
+
+export const deleteUser = async(req, res) =>{
+    try{
+        const {id} = req.params;
+        const user = await FormUser.findByIdAndDelete(id);
+        if(!user){
+            return res.status(404).json({ message: "User not found" })
+        }
+        res.status(200).json({ message: "User deleted successfully", user })
+    }catch(error){
+        res.status(500).json({ message: "Error deleting user", error })
+    }
+}
--- route/userRoute.js
+++ route/userRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { create, getAllUsers, getUserById, updateUser  } from '../controller/userController.js'
+import { create, getAllUsers, getUserById, updateUser, deleteUser } from '../controller/userController.js'
 
 const router = express.Router()
 
@@ -19,13 +19,12 @@ router.get('/:id', (req, res) => {
 
 //Update
 router.put('/:id', (req, res) => {
-    // res.json({ message: "User updated successfully" })
     updateUser(req, res)
 })
 
 //Delete
 router.delete('/:id', (req, res) => {
-    res.json({ message: "User deleted successfully" })
+    deleteUser(req, res)
 })
 
 export default router
